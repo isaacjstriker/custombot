29b0d1c529421cc58fef6931632f4e4a22b191a0
Isaac Striker <isaac@ezwebvt.com>
2025-12-24 09:32:51 -0500

    - Created env
    - Hard coded responses

---
+++ README.md
@@ -0,0 +1,13 @@
+# Custom Bot!
+
+## About
+
+---
+
+Creating an agent using an existing llm (gemini)
+
+## Goal
+
+---
+
+Understand how llm tools and agents work under the hood
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from custombot!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    # Load in env variables
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("API Key Invalid (is it missing?)")
+    
+    client = genai.Client(api_key=api_key)
+
+    # Hard coding the prompt
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    # Keep track of token usage
+    usage = response.usage_metadata
+
+    if usage.prompt_token_count is None:
+        raise RuntimeError("API Request failed. Please try again.")
+
+    # Check how many tokens we're using
+    print("Prompt tokens:", usage.prompt_token_count, "\nResponse tokens:", usage.candidates_token_count)
+
+    # Print the models response to the cli
+    print(response.text)
 
 if __name__ == "__main__":
     main()
